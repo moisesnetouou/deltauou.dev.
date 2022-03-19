8eb4bac8e73344987b0a3bbd6225ecd3725ced18
moises.neto <moisesnetored@gmail.com>
2022-03-19 01:28:40 -0400

    feat: Kbar, Stitches and React Icons added to the project.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import theme from "../styles/theme";
+
+import AppProvider from "../contexts";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
+    <AppProvider>
       <Component {...pageProps} />
-    </ChakraProvider>
+    </AppProvider>
   );
 }
 
